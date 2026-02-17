48fbcf876c5ca4841177ca8f6b09f91ac54408cb
4shwrya <aishwaryanegi3466@gmail.com>
2026-02-17 20:03:11 +0530

    UC1: Print literal OOPS to console

---
+++ PrintOOPS.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Aishwarya Negi
+ * @version 1.0
+ */
+public class PrintOOPS {
+
+    public static void main(String[] args) {
+
+        // UC1 Requirement: Print literal text OOPS
+        System.out.println("OOPS");
+
+    }
+}
