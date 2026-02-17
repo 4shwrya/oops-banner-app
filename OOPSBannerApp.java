362f906e7934a85bcbb0b1de49b40953f3b8ba1c
4shwrya <aishwaryanegi3466@gmail.com>
2026-02-17 15:46:50 +0530

    UC1: Print literal OOPS to console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,20 @@
+/**
+ * OOPSBannerApp
+ * Use Case 1: Print literal text "OOPS" to console
+ * Entry point of the application
+ *
+ * @author Aishwarya
+ * @version 1.0
+ */
 public class OOPSBannerApp {
+
+    /**
+     * Main method - application entry point
+     */
     public static void main(String[] args) {
 
-        System.out.println("  ***      ***      *****    ***** ");
-        System.out.println(" ** **    ** **     **  **   **    ");
-        System.out.println("**   **  **   **    **   **  **    ");
-        System.out.println("**   **  **   **    *****    ***   ");
-        System.out.println("**   **  **   **    **         **  ");
-        System.out.println(" ** **    ** **     **         **  ");
-        System.out.println("  ***      ***      **      *****  ");
+        // Print literal text
+        System.out.println("OOPS");
 
     }
 }
