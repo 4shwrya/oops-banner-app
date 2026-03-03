6494debb8695c19a3c3735263fe4be7e6da42912
4shwrya <aishwaryanegi3466@gmail.com>
2026-03-03 15:37:44 +0530

    UC6: Refactored banner using static helper methods (DRY principle)

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,51 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = new String[] {
-            String.join(" ", "O", "O", "O", "O", " ", "P", "P", "P", " ", "S", "S", "S"),
-            String.join(" ", "O", " ", " ", "O", " ", "P", " ", " ", "P", "S"),
-            String.join(" ", "O", " ", " ", "O", " ", "P", "P", "P", " ", "S", "S"),
-            String.join(" ", "O", " ", " ", "O", " ", "P", " ", " ", " ", " ", " ", "S"),
-            String.join(" ", "O", " ", " ", "O", " ", "P", " ", " ", " ", " ", " ", "S"),
-            String.join(" ", "O", " ", " ", "O", " ", "P", " ", " ", " ", "S"),
-            String.join(" ", "O", "O", "O", "O", " ", "P", " ", " ", " ", "S", "S", "S")
-        };
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = buildO(i) + "  " +
+                        buildO(i) + "  " +
+                        buildP(i) + "  " +
+                        buildS(i);
+        }
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // ----------- O Pattern -----------
+    public static String buildO(int row) {
+        if (row == 0 || row == 6)
+            return String.join(" ", "O","O","O","O");
+        else
+            return String.join(" ", "O"," "," ","O");
+    }
+
+    // ----------- P Pattern -----------
+    public static String buildP(int row) {
+        if (row == 0)
+            return String.join(" ", "P","P","P");
+        else if (row == 1)
+            return String.join(" ", "P"," "," ","P");
+        else if (row == 2)
+            return String.join(" ", "P","P","P");
+        else
+            return String.join(" ", "P");
+    }
+
+    // ----------- S Pattern -----------
+    public static String buildS(int row) {
+        if (row == 0 || row == 6)
+            return String.join(" ", "S","S","S");
+        else if (row == 2)
+            return String.join(" ", "S","S");
+        else if (row == 1)
+            return String.join(" ", "S");
+        else if (row == 3 || row == 4 || row == 5)
+            return String.join(" ", "S");
+        else
+            return "";
     }
 }
